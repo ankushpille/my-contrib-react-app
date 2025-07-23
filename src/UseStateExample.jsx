93c1e68c89ed71e0853a04dd8da6824226695e65
ankushpille <ankush.pille1@gmail.com>
2025-07-23 15:02:13 +0530

    controlled components using usestate with form inputs

--- src/UseStateExample.jsx
+++ src/UseStateExample.jsx
@@ -3,24 +3,34 @@ import {useState} from 'react'
 function UseStateExample(){
     const [count,setCount] = useState(0);
     const [toggled,setToggled] = useState(false);
+    const [name,setName] = useState("");
+     const [submittedName, setSubmittedName] = useState('');
+
 
     // const handleClick = () => {
     //     setCount(count +1)
     // }
 
-
-
-
-
-
-
+    const handleSubmit = (e) => {
+        setSubmittedName(name)
+    }
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+   
     return (
         <>
          {/* <button onClick={handleClick}>
             click me
          </button> */}
-          <button style={{width : '100px', color: toggled ? 'green' : 'red'}} onClick={() => setToggled(!toggled)}>toggle</button>
-          {}
+          {/* <button style={{width : '100px', color: toggled ? 'green' : 'red'}} onClick={() => setToggled(!toggled)}>toggle</button> */}
+          <input 
+          type="text" 
+          value={name}
+          onChange={handleChange}
+          />
+          <button onClick={handleSubmit}>submit</button>
+          <h1>{submittedName}</h1>
         </>
     )
 }
