be3a69be7f9992a059e153d9ce715984164cc434
ankushpille <ankush.pille1@gmail.com>
2025-07-23 14:23:02 +0530

    toggle using usestate practice

--- src/UseStateExample.jsx
+++ src/UseStateExample.jsx
@@ -2,18 +2,25 @@ import {useState} from 'react'
 
 function UseStateExample(){
     const [count,setCount] = useState(0);
+    const [toggled,setToggled] = useState(false);
 
-    const handleClick = () => {
-        setCount(count +1)
-    }
+    // const handleClick = () => {
+    //     setCount(count +1)
+    // }
+
+
+
+
+
+
 
     return (
         <>
-         <button onClick={handleClick}>
+         {/* <button onClick={handleClick}>
             click me
-         </button>
-         <h1>{count}</h1>
-         <h1>hey testing react</h1>
+         </button> */}
+          <button style={{width : '100px', color: toggled ? 'green' : 'red'}} onClick={() => setToggled(!toggled)}>toggle</button>
+          {}
         </>
     )
 }
